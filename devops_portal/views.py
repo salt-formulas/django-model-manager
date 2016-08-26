c415d3706b15feb6f74c41931b24f394f815b6e0
dresl <nikicresl@gmail.com>
2016-08-26 11:31:33 +0200

    fix

--- devops_portal/views.py
+++ devops_portal/views.py
@@ -7,40 +7,40 @@ from django.utils import timezone
 def overview(request):
     args = {}
 
-    return render(request, 'devops-portal/overview.html', args)
+    return render(request, 'devops_portal/overview.html', args)
 
 
 def jenkins(request):
     args = {}
 
-    return render(request, 'devops-portal/jenkins.html', args)
+    return render(request, 'devops_portal/jenkins.html', args)
 
 
 def gerrit(request):
     args = {}
 
-    return render(request, 'devops-portal/gerrit.html', args)
+    return render(request, 'devops_portal/gerrit.html', args)
 
 
 def maas(request):
     args = {}
 
-    return render(request, 'devops-portal/maas.html', args)
+    return render(request, 'devops_portal/maas.html', args)
 
 
 def artifactory(request):
     args = {}
 
-    return render(request, 'devops-portal/artifactory.html', args)
+    return render(request, 'devops_portal/artifactory.html', args)
 
 
 def salt(request):
     args = {}
 
-    return render(request, 'devops-portal/salt.html', args)
+    return render(request, 'devops_portal/salt.html', args)
 
 
 def doc(request):
     args = {}
 
-    return render(request, 'devops-portal/doc.html', args)
+    return render(request, 'devops_portal/doc.html', args)
