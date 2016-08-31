ddc2e77bb431c9a571ebc9f1a4e987d574378572
Ales Komarek <ales.komarek@tcpcloud.eu>
2016-08-31 12:05:14 +0200

    Params to iframe links

--- devops_portal/views.py
+++ devops_portal/views.py
@@ -1,3 +1,4 @@
+import os
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
@@ -5,31 +6,54 @@ from django.utils import timezone
 
 
 def overview(request):
-    args = {}
+
+    args = {
+        'GERRIT_HOST': os.getenv('DEVOPS_PORTAL_GERRIT_HOST', 'localhost'),
+        'GERRIT_PORT': os.getenv('DEVOPS_PORTAL_GERRIT_PORT', '8080'),
+        'JENKINS_HOST': os.getenv('DEVOPS_PORTAL_JENKINS_HOST', 'localhost'),
+        'JENKINS_PORT': os.getenv('DEVOPS_PORTAL_JENKINS_PORT', '8081'),
+        'MAAS_HOST': os.getenv('DEVOPS_PORTAL_MAAS_HOST', 'localhost'),
+        'MAAS_PORT': os.getenv('DEVOPS_PORTAL_MAAS_PORT', '80'),
+        'ARTIFACTORY_HOST': os.getenv('DEVOPS_PORTAL_ARTIFACTORY_HOST', 'localhost'),
+        'ARTIFACTORY_PORT': os.getenv('DEVOPS_PORTAL_ARTIFACTORY_PORT', '8082'),
+    }
 
     return render(request, 'devops_portal/overview.html', args)
 
 
 def jenkins(request):
-    args = {}
+
+    args = {
+        'JENKINS_HOST': os.getenv('DEVOPS_PORTAL_JENKINS_HOST', 'localhost'),
+        'JENKINS_PORT': os.getenv('DEVOPS_PORTAL_JENKINS_PORT', '8080'),
+    }
 
     return render(request, 'devops_portal/jenkins.html', args)
 
 
 def gerrit(request):
-    args = {}
+    args = {
+        'GERRIT_HOST': os.getenv('DEVOPS_PORTAL_GERRIT_HOST', 'localhost'),
+        'GERRIT_PORT': os.getenv('DEVOPS_PORTAL_GERRIT_PORT', '8080'),
+    }
 
     return render(request, 'devops_portal/gerrit.html', args)
 
 
 def maas(request):
-    args = {}
+    args = {
+        'MAAS_HOST': os.getenv('DEVOPS_PORTAL_MAAS_HOST', 'localhost'),
+        'MAAS_PORT': os.getenv('DEVOPS_PORTAL_MAAS_PORT', '80'),
+    }
 
     return render(request, 'devops_portal/maas.html', args)
 
 
 def artifactory(request):
-    args = {}
+    args = {
+        'ARTIFACTORY_HOST': os.getenv('DEVOPS_PORTAL_ARTIFACTORY_HOST', 'localhost'),
+        'ARTIFACTORY_PORT': os.getenv('DEVOPS_PORTAL_ARTIFACTORY_PORT', '8082'),
+    }
 
     return render(request, 'devops_portal/artifactory.html', args)
 
