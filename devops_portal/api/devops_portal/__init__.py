db71015bb4f79dd4ef0d4013ee060e3998ae4da8
Ales Komarek <ales.komarek@newt.cz>
2017-03-22 16:06:39 +0100

    Portal init

--- devops_portal/api/devops_portal/__init__.py
+++ devops_portal/api/devops_portal/__init__.py
@@ -1,10 +1,11 @@
 from .base import BaseClient
-from .jobs import Job
-from .users import User, Operator
+#from .jobs import Job
+#from .users import User, Operator
 
 class Api(BaseClient):
 
-    jobs = Job()
-    users = User()
+#    jobs = Job()
+#    users = User()
+    pass
 
 devops_portal = Api()
