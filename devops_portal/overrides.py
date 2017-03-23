61cd15f938ea9981c29fc8528b5cb0ae01c061af
Ales Komarek <ales.komarek@newt.cz>
2017-03-23 09:19:58 +0100

    fix short periods

--- devops_portal/overrides.py
+++ devops_portal/overrides.py
@@ -2,7 +2,7 @@ from django import http
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.debug import sensitive_variables  # noqa
-from devops_portal.api import devops_portal
+#from devops_portal.api import devops_portal
 from horizon import exceptions
 from horizon.utils import functions as utils
 #from openstack_dashboard.dashboards.settings.password import forms
