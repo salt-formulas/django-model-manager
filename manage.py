dc5c450963faeef1743ed1806ec054c0d9cb1b51
Adam Tengler <adam.tengler@tcpcloud.eu>
2017-03-24 15:54:52 +0100

    Create required files to runserver

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devops_portal.settings")
+    path = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(path)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devops_portal.base_settings")
 
     from django.core.management import execute_from_command_line
 
