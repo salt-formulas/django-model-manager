fe79b3cb9da84ac433a491415c765c48271952f5
Adam Tengler <adam.tengler@tcpcloud.eu>
2017-04-10 16:36:39 +0200

    Template fixes for cookiecutter and overview panels

--- devops_portal/dashboards/integration/overview/panel.py
+++ devops_portal/dashboards/integration/overview/panel.py
@@ -1,11 +1,7 @@
 import horizon
 from django.utils.translation import ugettext_lazy as _
-from devops_portal.dashboards.integration import dashboard
 
 
 class Overview(horizon.Panel):
     name = _("Overview")
     slug = 'overview'
-
-
-dashboard.Integration.register(Overview)
--- devops_portal/dashboards/integration/overview/views.py
+++ devops_portal/dashboards/integration/overview/views.py
@@ -1,14 +1,24 @@
 import json
 import logging
-from horizon import views
-from django.views.generic import TemplateView
+
 from django.http import HttpResponse
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import TemplateView
+from horizon import tables
+
+from .tables import OverviewTable
+
 
 LOG = logging.getLogger(__name__)
 
 
-class IndexView(views.HorizonTemplateView):
+class IndexView(tables.DataTableView):
+    table_class = OverviewTable
     template_name = "integration/overview/index.html"
+    page_title = _("Overview")
+
+    def get_data(self):
+        return {}
 
 
 class StatusView(TemplateView):
@@ -22,3 +32,4 @@ class StatusView(TemplateView):
 
         return HttpResponse(json.dumps(ret),
                             content_type='application/json')
+
