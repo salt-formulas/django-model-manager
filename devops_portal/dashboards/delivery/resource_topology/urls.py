6295b6ec012797f49b58eed02c633d3845743e65
Adam Tenger <adam.tengler@tcpcloud.eu>
2017-05-29 17:03:10 +0000

    Domain list table and detail views

--- devops_portal/dashboards/delivery/resource_topology/urls.py
+++ devops_portal/dashboards/delivery/resource_topology/urls.py
@@ -6,5 +6,6 @@ from devops_portal.dashboards.delivery.resource_topology import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^topology_data', views.topology_data_view, name='topology_data')
+    url(r'^topology_data/(?P<domain>[\w\.\-]+)$', views.topology_data_view, name='topology_data'),
+    url(r'^(?P<domain>[\w\.\-]+)$', views.DetailView.as_view(), name='detail')
 )
--- devops_portal/dashboards/delivery/resource_topology/views.py
+++ devops_portal/dashboards/delivery/resource_topology/views.py
@@ -1,33 +1,58 @@
 import logging
 
+from collections import Counter
 from devops_portal.api.salt import salt_client
 from django.conf import settings
 from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
 
-from horizon.views import HorizonTemplateView
+from horizon import tables
+from horizon import views
 
+from .tables import ResourceTopologyTable
+
+RECLASS_DOMAIN = getattr(settings, 'RECLASS_DOMAIN', '')
 LOG = logging.getLogger(__name__)
 
 
-class IndexView(HorizonTemplateView):
+class IndexView(tables.DataTableView):
+    table_class = ResourceTopologyTable
     template_name = "delivery/resource_topology/index.html"
     page_title = _("Resource Topology")
 
     def get_data(self):
-        return {}
+        data = []
+        res = salt_client.low([{'client': 'local', 'tgt': 'cfg01*', 'fun': 'reclass.inventory'}])
+        # unwrap response from Salt Master
+        inventory = res.get('return', [{'': ''}])[0].values()[0]
+        domains = [".".join(n.split('.')[1:]) for n in inventory]
+        counter = Counter(domains)
+        for domain, count in counter.items():
+            datum = {
+                'domain': domain,
+                'count': count
+            }
+            data.append(datum)
+
+        return data
+
+
+class DetailView(views.HorizonTemplateView):
+    template_name = "delivery/resource_topology/detail.html"
+    page_title = _("Resource Topology - {{ domain }}")
 
 
 def topology_data_view(self, *args, **kwargs):
+    data = []
     res = salt_client.low([{'client': 'local', 'tgt': 'cfg01*', 'fun': 'reclass.inventory'}])
     # unwrap response from Salt Master
     inventory = res.get('return', [{'': ''}])[0].values()[0]
-    data = []
-    reclass_cluster = getattr(settings, 'RECLASS_CLUSTER', '')
     # create topology graph source data, optionally filtered by cluster name
     for node, node_data in inventory.items():
-        if reclass_cluster in node:
+        if kwargs.get('domain', '') in node:
             roles = node_data.get('roles', [])
+            if kwargs.get('domain', None):
+                node = node.replace(kwargs.get('domain'), '')[:-1]
             for role in roles:
                 datum = {
                     'name': ".".join([node, role]),
