b179ae940eb0c1af3fb81a7189ea32dfe2d6993a
root <atengler@mirantis.com>
2018-04-12 19:10:49 +0000

    Create LinkAction for CookiecutterTable mapped to VersionForm modal view

--- model_manager/dashboards/integration/modeldesigner/urls.py
+++ model_manager/dashboards/integration/modeldesigner/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^create/$', views.CreateView.as_view(), name='create'),
+    url(r'^version/$', views.ContextVersionView.as_view(), name='version')
     #url(r'^(?P<build_id>[\w\.\-]+)/detail',
     #    views.DetailView.as_view(), name='detail'),
 )
