0e1d5a0fd656f337076549ba919dc3483d6cfcf2
Adam Tengler <atengler@mirantis.com>
2018-04-25 12:34:24 +0000

    Allow renaming of dynamically gathered versions through map in configuration file

--- model_manager/dashboards/integration/modeldesigner/utils.py
+++ model_manager/dashboards/integration/modeldesigner/utils.py
@@ -64,6 +64,7 @@ class ContextTemplateCollector(object):
         default_project_name = getattr(settings, 'COOKIECUTTER_CONTEXT_PROJECT_NAME', None)
         default_file_name = getattr(settings, 'COOKIECUTTER_CONTEXT_FILE_NAME', None)
         default_version_filter = getattr(settings, 'COOKIECUTTER_CONTEXT_VERSION_FILTER', None)
+        default_version_map = getattr(settings, 'COOKIECUTTER_CONTEXT_VERSION_MAP', {})
 
         self.url = kwargs.get('url', default_url)
         self.path = kwargs.get('path', default_path)
@@ -75,6 +76,7 @@ class ContextTemplateCollector(object):
         self.project_name = kwargs.get('project_name', default_project_name)
         self.file_name = kwargs.get('file_name', default_file_name)
         self.version_filter = kwargs.get('version_filter', default_version_filter)
+        self.version_map = kwargs.get('version_map', default_version_map)
 
         self.collectors = {
             'github': {
@@ -258,9 +260,17 @@ class ContextTemplateCollector(object):
     def collect_versions(self):
         collector = self.collectors.get(self.remote, {}).get('version_collector', lambda: [])
         versions = collector()
+
+        # filter versions by configured regular expression
         if self.version_filter:
             regex = re.compile(self.version_filter)
             versions = filter(regex.search, versions)
+
+        # replace version names by names configured in version map
+        for idx, version in enumerate(versions):
+            if version in self.version_map:
+                versions[idx] = self.version_map[version]
+
         return sorted(versions)
 
 
